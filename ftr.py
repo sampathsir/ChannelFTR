563d59f6cf37b3b4d032d2e841edd9b44ef09c16
sampathsir <83348785+sampathsir@users.noreply.github.com>
2021-08-27 10:38:21 +0530

    Update ftr.py

--- ftr.py
+++ ftr.py
@@ -17,8 +17,8 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 app = pyrogram.Client("app", api_id=APP_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
-but = InlineKeyboardMarkup([[InlineKeyboardButton("Help 🤔", callback_data="Help"), InlineKeyboardButton("Close 🔐", callback_data="close")],[InlineKeyboardButton("Updates ⬆", url="https://t.me/joinchat/wxtZQzEqGA9iYjc1"), InlineKeyboardButton("Developer", url="Satan_sinx")]])
-butt = InlineKeyboardMarkup([[InlineKeyboardButton("Home 🏠", callback_data="home"), InlineKeyboardButton("Updates ⬆", url="https://t.me/joinchat/wxtZQzEqGA9iYjc1")], [InlineKeyboardButton("ɴᴇᴛᴡᴏʀᴋ", url="https://t.me/The_Sinners_Empire"), InlineKeyboardButton("Close 🔐", callback_data="close")]])
+but = InlineKeyboardMarkup([[InlineKeyboardButton("Help 🤔", callback_data="Help"), InlineKeyboardButton("Close 🔐", callback_data="close")],[InlineKeyboardButton("Updates ⬆", url="https://t.me/sinnerBots"), InlineKeyboardButton("Developer", url="Satan_sinx")]])
+butt = InlineKeyboardMarkup([[InlineKeyboardButton("Home 🏠", callback_data="home"), InlineKeyboardButton("Updates ⬆", url="https://t.me/sinnerbots")], [InlineKeyboardButton("ɴᴇᴛᴡᴏʀᴋ", url="https://t.me/The_Sinners_Empire"), InlineKeyboardButton("Close 🔐", callback_data="close")]])
 
 
 @app.on_message(filters.command(["start"]))
@@ -52,5 +52,5 @@ async def copy(sed, message):
 
 
 print("Started bot...! ") 
-print("Join @Dengerousbots for any help !")
+print("Join @sinnerbots for any help !")
 app.run()
